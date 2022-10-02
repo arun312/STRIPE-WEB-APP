04a5f39d197e07dca28a7e45ac472b1faf12f3fc
Arun <arun@Aruns-MacBook-Pro.local>
2022-10-02 17:58:02 +0530

    changee ee

--- app.py
+++ app.py
@@ -23,6 +23,59 @@ app.config['SECRET_KEY']='mysecret'
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+
+@app.before_request 
+def before_request():
+    g.user= None
+    if 'user' in session:
+        g.user = session['user']
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    if g.user:
+        return redirect('/user_home') 
+    user=0
+    if request.method == 'POST':
+        email=request.form['email']
+        password=request.form['password']
+        query="SELECT user_id,user_name FROM USER WHERE user_email='%s' AND user_password='%s'"%(email,password)
+        detail=selection(query)
+        if detail!=False:
+            for d in detail:
+                session['user']=[d[0],d[1]]
+                return redirect('/user_home')  
+        return render_template('login.html',id='404',msg="INVALID CREDENTIALS")
+    try:
+        user=request.args.get('user')
+    except Exception as e:
+        print(e)
+    return render_template('login.html',user=user)
+###################### DATABASE ###################################
+
+def inUP(query): #Insertion or Updation Queries
+    try:
+        connection = sqlite3.connect('database.db')       
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+        connection.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def selection(query):  #Selection Queries
+    try:
+        connection = sqlite3.connect('database.db')       # Connect to the database
+        connection.row_factory = sqlite3.Row
+        cursor =  connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+        rv = cursor.fetchall()
+        connection.close()
+        return rv
+    except Exception as e:
+        print(e)
+        return False
+
+####################### DATABASE END###################################
