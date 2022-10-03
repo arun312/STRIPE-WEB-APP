aa137c8e413f4784f396c3a973b349315f18fc05
Arun <arun@Aruns-MacBook-Pro.local>
2022-10-03 11:38:54 +0530

    change ee eee ee

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import stripe
 # This is your test secret API key.
 stripe.api_key = 'sk_test_51LoYstSCQP9tLIEHALBPJAWNm4usoTNFQSAoujlYHyNQG4CnJ1RZsw8VLs0bhlShtaDn3ZSXzNzGULW255MUfLme00IVHSECqH'
 
-
+YOUR_DOMAIN = 'http://127.0.0.1:5000'
 app = Flask(__name__,
             static_url_path='', 
             static_folder='web/static')
@@ -26,15 +26,6 @@ app.config["upload_folder2"] = 'web/static/'
 app.config['SECRET_KEY']='mysecret'
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
-
-
-# This is your test secret API key.
-# stripe.api_key = 'sk_test_51LoYstSCQP9tLIEHALBPJAWNm4usoTNFQSAoujlYHyNQG4CnJ1RZsw8VLs0bhlShtaDn3ZSXzNzGULW255MUfLme00IVHSECqH'
-
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='public')
-
 
 @app.before_request 
 def before_request():
@@ -52,8 +43,10 @@ def user_home():
         query="SELECT * FROM PLAN"
         detail=selection(query)
         item=[]
+        count=0
         if detail!=False:
             for d in detail:
+                lst=[x for x in d[0]]
                 x={
                     'name':d[0],
                     'a':d[1],
@@ -62,9 +55,15 @@ def user_home():
                     'd':d[4],
                     'e':d[5],
                     'f':d[6],
+                    'fl':lst[0]
                 }
                 item.append(x)
-        return render_template('user_home.html',item=item)
+        query="SELECT COUNT(*) FROM PAYMENTS WHERE user_id='%s'"%(g.user[0])
+        detail=selection(query)
+        if detail!=False:
+            for d in detail:
+                count=d[0]
+        return render_template('user_home.html',item=item,count=count)
     return redirect('/login')
 
 @app.route("/login", methods=['GET','POST'])
@@ -107,11 +106,32 @@ def signup():
                     
     return render_template('register.html')
 
-@app.route("/logout",methods=['GET','POST'])                 #new
+@app.route("/logout",methods=['GET','POST'])                 
 def admin_logout():
     session['user'] = None
     return redirect('/')  
 
+@app.route("/user_manage",methods=['GET','POST'])                
+def user_manage():
+    if g.user:
+        if request.method=='POST':
+            query="DELETE FROM PAYMENTS WHERE payment_id='%s'"%(request.form['id'])
+            detail=inUP(query)
+            if detail!=False:
+                return redirect('/user_manage?id=200')
+        query="SELECT * FROM PAYMENTS WHERE user_id='%s'"%(g.user[0])
+        detail=selection(query)
+        item=[]
+        if detail!=False:
+            for d in detail:
+                x={
+                    'name':d[2],
+                    'id':d[0]
+                }
+                item.append(x)
+        return render_template('user_manage.html',item=item)
+
+    return redirect('/login')
 ###################### DATABASE ###################################
 
 def inUP(query): #Insertion or Updation Queries
@@ -141,3 +161,124 @@ def selection(query):  #Selection Queries
         return False
 
 ####################### DATABASE END###################################
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        prices = stripe.Price.list(
+            lookup_keys=[request.form['lookup_key']],
+            expand=['data.product']
+        )
+
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': prices.data[0].id,
+                    'quantity': 1,
+                },
+            ],
+            mode='subscription',
+            success_url=YOUR_DOMAIN +
+            '/success?session_id={CHECKOUT_SESSION_ID}&product='+request.form['lookup_key'],
+            cancel_url=YOUR_DOMAIN + '/cancel',
+        )
+        return redirect(checkout_session.url, code=303)
+    except Exception as e:
+        print(e)
+        return "Server error", 500
+
+@app.route('/create-portal-session', methods=['POST'])
+def customer_portal():
+    # For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
+    # Typically this is stored alongside the authenticated user in your database.
+    checkout_session_id = request.form.get('session_id')
+    checkout_session = stripe.checkout.Session.retrieve(checkout_session_id)
+
+    # This is the URL to which the customer will be redirected after they are
+    # done managing their billing with the portal.
+    return_url = YOUR_DOMAIN
+
+    portalSession = stripe.billing_portal.Session.create(
+        customer=checkout_session.customer,
+        return_url=return_url,
+    )
+    return redirect(portalSession.url, code=303)
+
+@app.route('/webhook', methods=['POST'])
+def webhook_received():
+    # Replace this endpoint secret with your endpoint's unique secret
+    # If you are testing with the CLI, find the secret by running 'stripe listen'
+    # If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+    # at https://dashboard.stripe.com/webhooks
+    webhook_secret = 'whsec_12345'
+    request_data = json.loads(request.data)
+
+    if webhook_secret:
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=webhook_secret)
+            data = event['data']
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event['type']
+    else:
+        data = request_data['data']
+        event_type = request_data['type']
+    data_object = data['object']
+
+    print('event ' + event_type)
+
+    if event_type == 'checkout.session.completed':
+        print('🔔 Payment succeeded!')
+    elif event_type == 'customer.subscription.trial_will_end':
+        print('Subscription trial will end')
+    elif event_type == 'customer.subscription.created':
+        print('Subscription created %s', event.id)
+    elif event_type == 'customer.subscription.updated':
+        print('Subscription created %s', event.id)
+    elif event_type == 'customer.subscription.deleted':
+        # handle subscription canceled automatically based
+        # upon your subscription settings. Or if the user cancels it.
+        print('Subscription canceled: %s', event.id)
+
+    return jsonify({'status': 'success'})
+
+##############
+@app.route("/checkout")
+def checkout():
+    key=request.args.get('key')
+    name=request.args.get('name')
+    price=request.args.get('price')
+    term=request.args.get('term')
+    return render_template('checkout.html',key=key,name=name,price=price,term=term)
+
+@app.route("/success")
+def success():
+    if g.user:
+        product=str(request.args.get('product'))
+        lst=[x for x in product]
+        if lst[0]=='B':
+            product='Basic'
+        elif lst[0]=='S':
+            product='Standard'
+        elif lst[0]=='P':
+            product='Premium'
+        elif lst[0]=='R':
+            product='Regular'
+        if lst[1]=='Y':
+            product=product+' Yearly'
+        elif lst[1]=='M':
+            product=product+' Monthly'
+        query="INSERT INTO 'PAYMENTS'(user_id,user_plan) values('%s','%s')"%(g.user[0],product)
+        detail=inUP(query)
+        if detail!=False:
+            return render_template('success.html')
+    else:
+        return redirect('/login')
+
+@app.route("/cancel")
+def cancel():
+    return render_template('cancel.html')
